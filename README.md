64e4ba00631ca58c085aba1e075a7e2021433ef8
ashvin.anilkumar <ashvinanilkumarsh@gmail.com>
2023-09-30 13:33:43 +0530

    Removed all unnecessary things

--- README.md
+++
@@ -1,2 +0,0 @@
-# Square-Helmholtz
-A 3d simulation of magnetic field produced by triple axis square helmholtz coil using python.
--- parameters.py
+++
@@ -1,6 +0,0 @@
-
-L = 0.6 #Length of side.
-rho = 1.724*pow(10,-8) #Resistivity of copper
-# Imax = test to see which account im in
-# Imin =
-# B =
